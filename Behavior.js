77b0b2bac777f9f8908af964ba56f6baec9ff12a
Seawyn <32650833+Seawyn@users.noreply.github.com>
2018-05-14 09:17:25 +0100

    Add files via upload

--- Behavior.js
+++ Behavior.js
@@ -4,13 +4,22 @@ var friendsdisplayed = 0;
 var currentScreen;                                                              //Variável que guarda o Id do ecrã em que o utilizador se encontra
 var stack = [];                                                                 //Variável que guarda os Ids dos ecrãs anteriores ao ecrã em que o utilizador se encontra
 
-var names = ["Ant&oacute;nio", "Ana", "Fernando", "Jo&atilde;o", "Jorge", "Pedro", "Rita", "Tiago", "Alice", "Mariana", "Teresa", "Paula", "In&ecirc;s", "Francisco", "Miguel"];
+var names = ["Ant&oacute;nio", "Ana", "Fernando", "Jo&atilde;o", "Jorge", "Pedro", "Rita", "Tiago", "Alice", "Mariana", "Teresa", "Paula", "In&ecirc;s", "Francisco", "Miguel", "Sofia"];
 var added = [];
 var toAdd;
 var addedLength = 0;
 var lastDisplayed = 0;
 var deleteMode = 0;
+
 var following1 = 0;
+var following2 = 0;
+var following3 = 0;
+var following4 = 0;
+
+var read1 = 0;
+var read2 = 0;
+var read3 = 0;
+var read4 = 0;
 
 var amount = 0;                                                                 //variavel que aumenta com os sinais de mais e menos
 var drink = 0;                                                                  //variavel que decide a bebida a ser apresentada
@@ -22,10 +31,11 @@ var onQueue = 0;
 
 var ArtistsDisplayed = 0;
 
-var LocationStatus = 1;
+var LocationStatus = 0;
+
+var angle = 0;
 
 function AddCart(){
-  window.alert("Dude what");
   var total = document.getElementById("Total");
   if(drink == 0){
     totalPint += amount;
@@ -215,7 +225,7 @@ function ScrollDetect(){
   var ScrollArea = document.getElementById("MenuScroll");
   var TopPart = document.getElementById("TopPart");
   var BottomPart = document.getElementById("BottomPart");
-  if (ScrollArea.scrollTop > 20){
+  if (ScrollArea.scrollTop > 50){
     TopPart.src = "Unoccupied.png";
     BottomPart.src = "Current.png";
   }
@@ -249,27 +259,6 @@ function IconDisplay(){
     Notificacoes.style.setProperty("visibility", "hidden");
     Definicoes.style.setProperty("visibility", "hidden");
     displayed = 0;
-  }
-}
-
-function ArtistDisplay(){
-  var Artist1 = document.getElementById("Artist1");
-  var Artist2 = document.getElementById("Artist2");
-  var Artist3 = document.getElementById("Artist3");
-  var Artist4 = document.getElementById("Artist4");
-  if (ArtistsDisplayed == 0){
-    Artist1.style.setProperty("visibility", "visible");
-    Artist2.style.setProperty("visibility", "visible");
-    Artist3.style.setProperty("visibility", "visible");
-    Artist4.style.setProperty("visibility", "visible");
-    ArtistsDisplayed = 1;
-  }
-  else{
-    Artist1.style.setProperty("visibility", "hidden");
-    Artist2.style.setProperty("visibility", "hidden");
-    Artist3.style.setProperty("visibility", "hidden");
-    Artist4.style.setProperty("visibility", "hidden");
-    ArtistsDisplayed = 0;
   }
 }
 
@@ -307,9 +296,6 @@ function Startup(){
 }
 
 function Switch(current, toSwitch){
-  if (current.id == 'DrinksMenu' && toSwitch.id == 'PaymentMenu' && totalDrinks == 0){
-    return;
-  }
   current.style.setProperty("visibility", "hidden");
   toSwitch.style.setProperty("visibility", "visible");
   if (current == 'MainScreen' || toSwitch == 'MainScreen'){
@@ -321,14 +307,21 @@ function Switch(current, toSwitch){
     var Pel = document.getElementById("Present");
     Present.style.setProperty("visibility", "visible");
   }
-
   if (toSwitch.id == 'WifiAwait'){
     if (LocationStatus != 0) {
-      setTimeout(function() { Switch(WifiAwait, SuccessfulConnection); }, 1000);
+      setTimeout(function() { Switch(WifiAwait, AddChoice); }, 1000);
     }
     else {
       verifyLocation();
     }
+  }
+  if (toSwitch.id == 'AddChoice'){
+    giveRandom();
+  }
+  if (toSwitch.id == 'LocationMenu' && current.id == 'AddChoice'){
+    stack.pop();
+    stack.pop();
+    stack.pop();
   }
   if (toSwitch.id == 'SuccessfulConnection'){
     if (LocationStatus != 0){
@@ -342,6 +335,7 @@ function Switch(current, toSwitch){
     stack.pop();
     stack.pop();
     stack.pop();
+    stack.pop();
   }
   if (toSwitch.id == 'Locator'){
     if (LocationStatus != 0){
@@ -350,6 +344,10 @@ function Switch(current, toSwitch){
     else {
       verifyLocation();
     }
+  }
+  if (toSwitch.id == 'Friends'){
+    var Scroll = document.getElementById("FriendsScroll");
+    Scroll.style.setProperty("visibility", "visible");
   }
   if (toSwitch.id != 'Locator' && friendsdisplayed == 1){
     hideFriends();
@@ -378,16 +376,101 @@ function Switch(current, toSwitch){
       button.style.setProperty("top", "-17.5%");
     }
   }
+  if (current.id == 'WCSuccessful'){
+    stack.pop();
+    stack.pop();
+  }
+  if (toSwitch.id == 'Notifications'){
+    verifyNotifications();
+  }
   if (toSwitch.id == 'News'){
     var ScrollArea = document.getElementById("NewsScroll");
     ScrollArea.style.setProperty("visibility", "visible");
+    verifyNotifications();
+    if (following3 == 1){
+      var NewsDiv = document.getElementById("NewsDiv1");
+      NewsDiv.style.setProperty("visibility", "visible");
+      if (read1 == 0){
+        NewsDiv.style.setProperty("background-color", "#abc653");
+      }
+    }
+    if (following2 == 1){
+      var NewsDiv = document.getElementById("NewsDiv2");
+      NewsDiv.style.setProperty("visibility", "visible");
+      if (read2 == 0){
+        NewsDiv.style.setProperty("background-color", "#abc653");
+      }
+    }
+    if (following1 == 1){
+      var NewsDiv = document.getElementById("NewsDiv3");
+      NewsDiv.style.setProperty("visibility", "visible");
+      if (read3 == 0){
+        NewsDiv.style.setProperty("background-color", "#abc653");
+      }
+    }
+    if (following4 == 1){
+      var NewsDiv = document.getElementById("NewsDiv4");
+      NewsDiv.style.setProperty("visibility", "visible");
+      if (read4 == 0){
+        NewsDiv.style.setProperty("background-color", "#abc653");
+      }
+    }
+  }
+  if (toSwitch.id == 'NewsArtist1'){
+    var ScrollArea = document.getElementById("NewsScroll1");
+    ScrollArea.style.setProperty("visibility", "visible");
+    read3 = 1;
+    verifyNotifications();
+    var NewsDiv = document.getElementById("NewsDiv3");
+    NewsDiv.style.setProperty("background-color", "#b9cf6e");
+  }
+  if (toSwitch.id == 'NewsArtist2'){
+    var ScrollArea = document.getElementById("NewsScroll2");
+    ScrollArea.style.setProperty("visibility", "visible");
+    read1 = 1;
+    verifyNotifications();
+    var NewsDiv = document.getElementById("NewsDiv1");
+    NewsDiv.style.setProperty("background-color", "#b9cf6e");
+  }
+  if (toSwitch.id == 'NewsArtist3'){
+    var ScrollArea = document.getElementById("NewsScroll3");
+    ScrollArea.style.setProperty("visibility", "visible");
+    read2 = 1;
+    verifyNotifications();
+    var NewsDiv = document.getElementById("NewsDiv2");
+    NewsDiv.style.setProperty("background-color", "#b9cf6e");
+  }
+  if (toSwitch.id == 'NewsArtist4'){
+    var ScrollArea = document.getElementById("NewsScroll4");
+    ScrollArea.style.setProperty("visibility", "visible");
+    read4 = 1;
+    verifyNotifications();
+    var NewsDiv = document.getElementById("NewsDiv4");
+    NewsDiv.style.setProperty("background-color", "#b9cf6e");
   }
   if (toSwitch.id == 'Playbill'){
-    ArtistDisplay();
-    var Pel = document.getElementById("Present");
-    Pel.style.setProperty("color", "#b9cf6e");
     var ScrollArea = document.getElementById("PlaybillScroll");
     ScrollArea.style.setProperty("visibility", "visible");
+  }
+  if (toSwitch.id == 'Artist1'){
+    var Scroll = document.getElementById("ArtistScroll1");
+    Scroll.style.setProperty("visibility", "visible");
+  }
+  if (toSwitch.id == 'Artist2'){
+    var Scroll = document.getElementById("ArtistScroll2");
+    Scroll.style.setProperty("visibility", "visible");
+  }
+  if (toSwitch.id == 'Artist3'){
+    var Scroll = document.getElementById("ArtistScroll3");
+    Scroll.style.setProperty("visibility", "visible");
+  }
+  if (toSwitch.id == 'Artist4'){
+    var Scroll = document.getElementById("ArtistScroll4");
+    Scroll.style.setProperty("visibility", "visible");
+  }
+  if (toSwitch.id == 'Artist1' || toSwitch.id == 'Artist2' || toSwitch.id == 'Artist3' || toSwitch.id == 'Artist4'){
+    var Pel = document.getElementById("Present");
+    Pel.style.setProperty("color", "#b9cf6e");
   }
   else {
     var Pel = document.getElementById("Present");
@@ -406,33 +489,80 @@ function Goback(){
     if (toSwitch.id != 'Locator' && friendsdisplayed == 1){
       hideFriends();
     }
+    if (currentScreen.id == 'Friends'){
+      var Scroll = document.getElementById("FriendsScroll");
+      Scroll.style.setProperty("visibility", "hidden");
+    }
+    if (currentScreen.id == 'Notifications'){
+      var image = document.getElementById("NewNotifications");
+      image.style.setProperty("visibility", "hidden");
+    }
     if (currentScreen.id == 'News'){
       var ScrollArea = document.getElementById("NewsScroll");
       ScrollArea.style.setProperty("visibility", "hidden");
+      var Newsdiv1 = document.getElementById("NewsDiv1");
+      Newsdiv1.style.setProperty("visibility", "hidden");
+      var Newsdiv2 = document.getElementById("NewsDiv2");
+      Newsdiv2.style.setProperty("visibility", "hidden");
+      var Newsdiv3 = document.getElementById("NewsDiv3");
+      Newsdiv3.style.setProperty("visibility", "hidden");
+      var Newsdiv4 = document.getElementById("NewsDiv4");
+      Newsdiv4.style.setProperty("visibility", "hidden");
+    }
+    if (currentScreen.id == 'NewsArtist1'){
+      var ScrollArea = document.getElementById("NewsScroll1");
+      ScrollArea.style.setProperty("visibility", "hidden");
+    }
+    if (currentScreen.id == 'NewsArtist2'){
+      var ScrollArea = document.getElementById("NewsScroll2");
+      ScrollArea.style.setProperty("visibility", "hidden");
+    }
+    if (currentScreen.id == 'NewsArtist3'){
+      var ScrollArea = document.getElementById("NewsScroll3");
+      ScrollArea.style.setProperty("visibility", "hidden");
+    }
+    if (currentScreen.id == 'NewsArtist4'){
+      var ScrollArea = document.getElementById("NewsScroll4");
+      ScrollArea.style.setProperty("visibility", "hidden");
     }
     if (currentScreen.id == 'Playbill'){
-      ArtistDisplay();
-      var Pel = document.getElementById("Present");
-      Pel.style.setProperty("color", "black");
       var ScrollArea = document.getElementById("PlaybillScroll");
       ScrollArea.style.setProperty("visibility", "hidden");
+      verifyNotifications();
+    }
+    if (currentScreen.id == 'Artist1' || currentScreen.id == 'Artist2' || currentScreen.id == 'Artist3' || currentScreen.id == 'Artist4'){
+      var Pel = document.getElementById("Present");
+      Pel.style.setProperty("color", "black");
+    }
+    if (currentScreen.id == 'Artist1'){
+      var Scroll = document.getElementById("ArtistScroll1");
+      Scroll.style.setProperty("visibility", "hidden");
+    }
+    if (currentScreen.id == 'Artist2'){
+      var Scroll = document.getElementById("ArtistScroll2");
+      Scroll.style.setProperty("visibility", "hidden");
+    }
+    if (currentScreen.id == 'Artist3'){
+      var Scroll = document.getElementById("ArtistScroll3");
+      Scroll.style.setProperty("visibility", "hidden");
+    }
+    if (currentScreen.id == 'Artist4'){
+      var Scroll = document.getElementById("ArtistScroll4");
+      Scroll.style.setProperty("visibility", "hidden");
     }
     currentScreen = toSwitch;
   }
 }
 
-function Display(Name){
-  var message = Name + " will be displayed when I get to it."
-  window.alert(message);
-}
-
 function giveRandom(){
-  var name = names[Math.floor((Math.random() * 14) + 1)];
+  var name = names[Math.floor((Math.random() * 15) + 1)];
+  var Place = document.getElementById("AddQuestion");
   toAdd = name;
+  var text = "Tem a certeza que deseja adicionar " + toAdd + "?";
+  Place.innerHTML = text;
 }
 
 function addFriend(){
-  giveRandom();
   added[addedLength] = toAdd;
   Place = document.getElementById("Placer");
   Place.style.setProperty("top", "0%");
@@ -442,13 +572,11 @@ function addFriend(){
   addedLength++;
   HText = document.getElementById("Header");
   HText.innerHTML = toAdd + " foi adicionado(a)!";
-  if (addedLength < 7){
-    if(addedLength == 0){
-      Place.innerHTML = toAdd + "<br>";
-    }
-    else
-      Place.innerHTML = Place.innerHTML + toAdd + "<br>";
+  if(addedLength == 0){
+    Place.innerHTML = toAdd + "<br>";
   }
+  else
+    Place.innerHTML = Place.innerHTML + toAdd + "<br>";
 }
 
 function displayFriend(){
@@ -495,15 +623,91 @@ function verifyLocation(){
   }
 }
 
-function followArtist1(){
-  var image = document.getElementById("Box1");
-  if (!following1){
-    image.src = "CheckmarkInBox.png";
-    following1 = 1;
+function followArtist(number){
+  if (number == 1){
+    var image = document.getElementById("ArtistBox1");
+    var news = document.getElementById("News3");
+    var bar = document.getElementById("NewsBar3");
+    if (!following1){
+      image.src = "CheckmarkInBox.png";
+      following1 = 1;
+      news.style.setProperty("display", "inline");
+      bar.style.setProperty("display", "block");
+    }
+    else {
+      image.src = "Box.png";
+      following1 = 0;
+      news.style.setProperty("display", "none");
+      bar.style.setProperty("display", "none");
+    }
+  }
+  else if (number == 2){
+    var image = document.getElementById("ArtistBox2");
+    var news = document.getElementById("News2");
+    var bar = document.getElementById("NewsBar2");
+    if (!following2){
+      image.src = "CheckmarkInBox.png";
+      following2 = 1;
+      news.style.setProperty("display", "inline");
+      bar.style.setProperty("display", "block");
+    }
+    else {
+      image.src = "Box.png";
+      following2 = 0;
+      news.style.setProperty("display", "none");
+      bar.style.setProperty("display", "none");
+    }
+  }
+  else if (number == 3){
+    var image = document.getElementById("ArtistBox3");
+    var news = document.getElementById("News1");
+    var bar = document.getElementById("NewsBar1");
+    if (!following3){
+      image.src = "CheckmarkInBox.png";
+      following3 = 1;
+      news.style.setProperty("display", "inline");
+      bar.style.setProperty("display", "block");
+    }
+    else {
+      image.src = "Box.png";
+      following3 = 0;
+      news.style.setProperty("display", "none");
+      bar.style.setProperty("display", "none");
+    }
   }
   else {
-    image.src = "Box.png";
-    following1 = 0;
+    var image = document.getElementById("ArtistBox4");
+    var news = document.getElementById("News4");
+    var bar = document.getElementById("NewsBar4");
+    if (!following4){
+      image.src = "CheckmarkInBox.png";
+      following4 = 1;
+      news.style.setProperty("display", "inline");
+      bar.style.setProperty("display", "block");
+    }
+    else {
+      image.src = "Box.png";
+      following4 = 0;
+      news.style.setProperty("display", "none");
+      bar.style.setProperty("display", "none");
+    }
+  }
+  var header = document.getElementById("NewsHeader");
+  if (following1 == 1 || following2 == 1 || following3 == 1 || following4 == 1) {
+    header.style.setProperty("display", "none");
+  }
+  else {
+    header.style.setProperty("display", "inline");
+  }
+}
+
+function verifyNotifications(){
+  var image = document.getElementById("NewNotifications");
+  if ((read1 == 0 && following3 == 1) || (read2 == 0 && following2 == 1) || (read3 == 0 && following1 == 1) || (read4 == 0 && following4 == 1)){
+    image.style.setProperty("visibility", "visible");
+  }
+  else {
+    image.style.setProperty("visibility", "hidden");
   }
 }
 
